427edee59dbc6a6a6c520963766cac3a0d4cc004
MateuszStepien1104 <mateusz.stepien@city.ac.uk>
2024-03-15 13:59:15 +0000

    Complete start method of TemporaryNode

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -7,6 +7,9 @@
 // Mateusz.Stepien@city.ac.uk
 
 
+import java.io.*;
+import java.net.Socket;
+
 // DO NOT EDIT starts
 interface TemporaryNodeInterface {
     public boolean start(String startingNodeName, String startingNodeAddress);
@@ -17,24 +20,47 @@ interface TemporaryNodeInterface {
 
 
 public class TemporaryNode implements TemporaryNodeInterface {
+    private BufferedReader reader;
+    private Writer writer;
+    private Socket clientSocket;
     public boolean start(String startingNodeName, String startingNodeAddress) {
-	// Implement this!
-	// Return true if the 2D#4 network can be contacted
-	// Return false if the 2D#4 network can't be contacted
-	return true;
+        // Implement this!
+        // Return true if the 2D#4 network can be contacted
+        // Return false if the 2D#4 network can't be contacted
+
+        String[] addressParts = startingNodeAddress.split(":");
+        try {
+            clientSocket = new Socket(addressParts[0], Integer.parseInt(addressParts[1]));
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            writer = new OutputStreamWriter(clientSocket.getOutputStream());
+
+            String startMsg = "START 1" + startingNodeName + "\n";
+            writer.write(startMsg);
+            writer.flush();
+
+            String rsp = reader.readLine();
+            if (!rsp.contains("START")) {
+                clientSocket.close();
+                return false;
+            }
+            return true;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
     }
 
     public boolean store(String key, String value) {
-	// Implement this!
-	// Return true if the store worked
-	// Return false if the store failed
-	return true;
+	    // Implement this!
+	    // Return true if the store worked
+	    // Return false if the store failed
+	    return true;
     }
 
     public String get(String key) {
-	// Implement this!
-	// Return the string if the get worked
-	// Return null if it didn't
-	return "Not implemented";
+	    // Implement this!
+	    // Return the string if the get worked
+	    // Return null if it didn't
+	    return "Not implemented";
     }
 }
