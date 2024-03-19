8b125066847e452acbb433a9fc37b55faedddae0
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-03-19 21:38:05 +0000

    Add comments

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -25,39 +25,37 @@ public class TemporaryNode implements TemporaryNodeInterface {
     private Writer writer;
 
     public boolean start(String startingNodeName, String startingNodeAddress) {
-        // Implement this!
-        // Return true if the 2D#4 network can be contacted
-        // Return false if the 2D#4 network can't be contacted
         try {
 
+            // Split the address and initialise the socket, reader, and writer
             String[] addressParts = startingNodeAddress.split(":");
             clientSocket = new Socket(addressParts[0], Integer.parseInt(addressParts[1]));
             reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             writer = new OutputStreamWriter(clientSocket.getOutputStream());
 
+            // Create and send a START message
             String message = "START 1" + startingNodeName;
             writer.write(message);
             writer.flush();
 
+            // Receive and check the response
             String response = reader.readLine();
             if (!response.startsWith("START")) {
                 clientSocket.close();
-                return false;
+                return false; // 2D#4 network can't be contacted
             }
 
         } catch (IOException e) {
             System.err.println(e.getMessage());
             return false;
         }
-        return true;
+        return true; // 2D#4 network can be contacted
     }
 
     public boolean store(String key, String value) {
-	    // Implement this!
-	    // Return true if the store worked
-	    // Return false if the store failed
         try {
 
+            // Count the lines in the key and value, and send a PUT request
             long keyLines = key.chars().filter(ch -> ch == '\n').count();
             long valLines = value.chars().filter(ch -> ch == '\n').count();
             String request = "PUT? " + keyLines + ' ' + valLines + '\n'
@@ -66,34 +64,34 @@ public class TemporaryNode implements TemporaryNodeInterface {
             writer.write(request);
             writer.flush();
 
+            // Receive and check the response
             String response = reader.readLine();
-            return response.equals("SUCCESS");
+            return response.equals("SUCCESS"); // SUCCESS -> worked; FAILED -> failed
 
         } catch (IOException e) {
             System.err.println(e.getMessage());
-            return false;
+            return false; // The STORE failed
         }
     }
 
     public String get(String key) {
-	    // Implement this!
-	    // Return the string if the get worked
-	    // Return null if it didn't
-        String value = null;
+        String value = null; // Return null if the GET failed
         try {
 
+            // Count the lines in the key, and send a GET request for the value
             long keyLines = key.chars().filter(ch -> ch == '\n').count();
             String request = "GET? " + keyLines + '\n' + key;
             writer.write(request);
             writer.flush();
 
+            // Receive and check the response
             String response = reader.readLine();
-            if (response.startsWith("VALUE"))
-                value = response.substring(response.indexOf('\n') + 1);
+            if (response.startsWith("VALUE")) // VALUE... -> worked; NOPE -> failed
+                value = response.substring(response.indexOf('\n') + 1); // The string value is a substring
 
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        return value;
+        return value; // The GET worked/failed
     }
 }
