d515729f1b1372114581a8167e1cde92ec5c3356
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-03-26 15:53:50 +0000

    Add test classes

--- src/FullNodeTest.java
+++ src/FullNodeTest.java
@@ -1,7 +1,7 @@
 public class FullNodeTest {
     public static void main(String[] args) {
         FullNode server = new FullNode();
-        server.listen("127.0.0.1", 2345);
-        server.handleIncomingConnections("mateusz.stepien@city.ac.uk:MyNode", "127.0.0.1:2345");
+        if (server.listen("127.0.0.1", 2345))
+            server.handleIncomingConnections("mateusz.stepien@city.ac.uk:MyNode", "127.0.0.1:2345");
     }
 }
--- src/TemporaryNodeTest.java
+++ src/TemporaryNodeTest.java
@@ -1,7 +1,9 @@
 public class TemporaryNodeTest {
     public static void main(String[] args) {
         TemporaryNode client = new TemporaryNode();
-        client.start("mateusz.stepien@city.ac.uk:MyNode", "127.0.0.1:2345");
-        client.store("Welcome\n", "Hello\nWorld\n");
+        if (client.start("mateusz.stepien@city.ac.uk:MyNode", "127.0.0.1:2345"))
+            System.out.println(" -> START worked <-");
+        if (client.store("Welcome", "Hello World"))
+            System.out.println(" -> STORE worked <-");
     }
 }
