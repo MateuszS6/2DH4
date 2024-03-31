add4ac39e58c550d25bc58c6289133e6739a9749
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-03-31 02:06:17 +0100

    Update HashID.java
    Bytes to hex, generate hash, calculate distance between 2 hashes

--- src/HashID.java
+++ src/HashID.java
@@ -3,13 +3,19 @@
 //
 // Construct the hashID for a string
 
-import java.lang.StringBuilder;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 public class HashID {
 	public static void main(String[] args) {
-		// TODO: test hashing
+		String s1 = "Hello World!";
+		String s2 = "martin.brain@city.ac.uk:MyCoolImplementation,1.41,test-node-2";
+		System.out.println(s1 + "\n\t> " + generate(s1));
+		System.out.println(s2 + "\n\t> " + generate(s2));
+		String h1 = "0f033be6cea034bd45a0352775a219ef5dc7825ce55d1f7dae9762d80ce64411";
+		String h2 = "0f0139b167bb7b4a416b8f6a7e0daa7e24a08172b9892171e5fdc615bb7f999b";
+		System.out.println(h1 + '\n' + h2 + "\n\t> Distance: " + calculateDistance(h1, h2));
 	}
 
     public static byte[] computeHashID(String line) throws Exception {
@@ -25,7 +31,31 @@ public class HashID {
     }
 
 	public static String bytesToHex(byte[] bytes) {
-
-        return null;
+		BigInteger bigInt = new BigInteger(1, bytes);
+		String hex = bigInt.toString(16);
+		int paddingLength = (bytes.length * 2) - hex.length();
+		if (paddingLength > 0) {
+			return String.format("%0" + paddingLength + "d", 0) + hex;
+		} else {
+			return hex;
+		}
     }
+
+	public static String generate(String line) {
+        try {
+			if (!line.endsWith("\n")) line += '\n';
+            return bytesToHex(computeHashID(line));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+	public static int calculateDistance(String hashID1, String hashID2) {
+		BigInteger bigInt1 = new BigInteger(hashID1, 16);
+		BigInteger bigInt2 = new BigInteger(hashID2, 16);
+		BigInteger distance = bigInt1.xor(bigInt2);
+		if (distance.equals(BigInteger.ZERO)) return 0;
+		String binaryDistance = distance.toString(2);
+        return binaryDistance.length();
+	}
 }
