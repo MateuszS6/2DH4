31bd37ef34345635e7584979513376bb2c19a511
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-04-24 23:53:55 +0100

    NEAREST? check in get method

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -9,6 +9,10 @@
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 // DO NOT EDIT starts
 interface TemporaryNodeInterface {
@@ -23,6 +27,9 @@ public class TemporaryNode implements TemporaryNodeInterface {
     private Socket socket;
     private BufferedReader in;
     private BufferedWriter out;
+    private String currentFullNodeName;
+    private String currentFullNodeAddress;
+    private final Map<String, Socket> connections = new HashMap<>();
 
     public static void main(String[] args) {
         TemporaryNode client = new TemporaryNode();
@@ -39,84 +46,105 @@ public class TemporaryNode implements TemporaryNodeInterface {
 
     public boolean start(String startingNodeName, String startingNodeAddress) {
         try {
-            // Split the address and open a client socket
+            // Split the address
             String[] addressParts = startingNodeAddress.split(":");
-            socket = new Socket(addressParts[0], Integer.parseInt(addressParts[1]));
-            System.out.println(" --- Connected!");
 
-            // Create the reader and writer
+            // Initialise fields
+            socket = new Socket(addressParts[0], Integer.parseInt(addressParts[1]));
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            currentFullNodeName = startingNodeName;
+            currentFullNodeAddress = startingNodeAddress;
 
-            // Create and send a START request
-            String request = "START 1 " + startingNodeName;
-            Node.send(out, request);
-
-            // Receive and check the response
-            String response = Node.readNextLine(in); // START... (same as sent) -> worked
-            if (!response.equals(request)) {
+            // Send a request and check the response
+            String response = Node.sendStartRequest(in, out, startingNodeName);
+            if (response.startsWith("START")) {
+                // Add to known nodes
+//            connections.put(nodeAddress, socket);
+                System.out.println(" --- Connected!");
+                return true; // 2D#4 network can be contacted
+            } else {
                 if (response.equals("END")) handleEnd();
-                else Node.end(out, "Unexpected response");
+                else Node.sendEndRequest(out, "Unexpected response");
                 return false; // 2D#4 network can't be contacted
             }
-            Node.send(out, "NOTIFY?\n" + startingNodeName + '\n' + startingNodeAddress);
-            if (!Node.readNextLine(in).equals("NOTIFIED")) Node.end(out, "Unexpected request");
-
-            return true; // 2D#4 network can be contacted
         } catch (IOException e) {
             System.err.println(e.getMessage());
             return false;
         }
+
+//        Node.send(out, "NOTIFY?\n" + startingNodeName + '\n' + startingNodeAddress);
+//        if (!Node.readNextLine(in).equals("NOTIFIED")) Node.end(out, "Unexpected request");
     }
 
     public boolean store(String key, String value) {
-        // Split the key and value into individual words
-        String[] keyParts = key.split("\n");
-        if (keyParts.length < 1) System.err.println("Blank key entered.");
-        String[] valueParts = value.split("\n");
-        if (valueParts.length < 1) System.err.println("Blank value entered.");
-
-//        String closestNode = findClosestNode(key);
-
-        // Send PUT request
-        Node.send(out, "PUT? " + keyParts.length + ' ' + valueParts.length + '\n' + key + value);
-
-        // Receive and check the response
-        String response = Node.readNextLine(in);
-        if (!response.equals("SUCCESS")) {
+        // Send a request and check the response
+        String response = Node.sendPutRequest(in, out, key, value);
+        if (response.equals("SUCCESS")) return true; // SUCCESS -> worked
+        else {
             if (response.equals("FAILED")) System.err.println("Not implemented.");
             else if (response.equals("END")) handleEnd();
-            else Node.end(out, "Unexpected response");
-        } else return true; // SUCCESS -> worked
-
+            else Node.sendEndRequest(out, "Unexpected response");
+        }
         return false; // FAILED -> failed
     }
 
     public String get(String key) {
         String value = null; // Return null if the GET failed
+        List<FullNodeInfo> visitedNodes = new ArrayList<>();
 
-        // Split the key into individual words
-        String[] keyParts = key.split("\n");
-        if (keyParts.length < 1) System.err.println("Empty key entered.");
-
-        // Send GET request
-        Node.send(out, "GET? " + keyParts.length + '\n' + key);
-
-        // Receive and check the response
-        String response = Node.readNextLine(in);
-        if (!response.startsWith("VALUE")) {
-            if (response.equals("NOPE")) System.err.println("Not implemented."); // TODO: Find next closest nodes
-            else if (response.equals("END")) handleEnd();
-            else Node.end(out, "Unexpected response");
-        } else {
+        // Send a request and check the response
+        String response = Node.sendGetRequest(in, out, key);
+        while (response.startsWith("NOPE")) {
+            List<FullNodeInfo> nodes = Node.sendNearestRequest(in, out, HashID.generate(key));
+            boolean found = false;
+            for (FullNodeInfo nodeInfo : nodes) {
+                if (!visitedNodes.contains(nodeInfo)) {
+                    if (start(nodeInfo.getName(), nodeInfo.getAddress())) {
+                        response = Node.sendGetRequest(in, out, key);
+                        visitedNodes.add(nodeInfo);
+                        if (response.startsWith("VALUE")) {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (found) break; // Exit the loop if the value is found
+        }
+        if (response.startsWith("VALUE")) {
             int valueLines = Integer.parseInt(response.split(" ")[1]);
             StringBuilder valueBuilder = new StringBuilder();
-            for (int v = 0; v < valueLines; v++) valueBuilder.append(Node.readNextLine(in)).append('\n');
+            for (int v = 0; v < valueLines; v++)
+                valueBuilder.append(Node.readNextLine(in)).append('\n');
             value = valueBuilder.toString();
+        } else {
+            if (response.equals("END")) handleEnd();
+            else Node.sendEndRequest(out, "Unexpected response");
         }
 
         return value;
     }
+
+    /*
+    public String findClosestNode(String key) {
+        String[] nodes = Node.sendNearestRequest(in, out, HashID.generate(key));
+        String closest = nodes[0];
+        if (nodes.length > 1) {
+            String nodeName = closest.split(" ")[0];
+            int lowestDistance = HashID.calculateDistance(key, nodeName);
+            for (int i = 1; i < nodes.length; i++) {
+                nodeName = nodes[i].split(" ")[0];
+                int distance = HashID.calculateDistance(key, nodeName);
+                if (distance < lowestDistance) {
+                    lowestDistance = distance;
+                    closest = nodes[i];
+                }
+            }
+        }
+        return closest;
+    }
+    */
 
     public void handleEnd() {
         try {
@@ -127,23 +155,5 @@ public class TemporaryNode implements TemporaryNodeInterface {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public String findClosestNode(String string) {
-        String[] nodes = Node.nearest(in, out, HashID.generate(string));
-        String closest = nodes[0];
-        if (nodes.length > 1) {
-            String nodeName = closest.split(" ")[0];
-            int lowestDistance = HashID.calculateDistance(string, nodeName);
-            for (int i = 1; i < nodes.length; i++) {
-                nodeName = nodes[i].split(" ")[0];
-                int distance = HashID.calculateDistance(string, nodeName);
-                if (distance < lowestDistance) {
-                    lowestDistance = distance;
-                    closest = nodes[i];
-                }
-            }
-        }
-        return closest;
     }
 }
