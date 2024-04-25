8c490b7f006ca5aefa4fd90d71059c6fe1687c7c
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-04-26 00:34:15 +0100

    Utility methods for all request types

--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Node {
     public static void send(BufferedWriter writer, String string) {
@@ -26,23 +27,67 @@ public class Node {
         return line;
     }
 
-    public static String[] nearest(BufferedReader reader, BufferedWriter writer, String hashID) {
-        String[] nodes = new String[3];
-        Node.send(writer, "NEAREST? " + hashID);
-        String response = Node.readNextLine(reader);
-        if (!response.startsWith("NODES")) System.err.println("Unexpected response."); // TODO: END
-        else {
-            int number = Integer.parseInt(response.split(" ")[1]);
-            for (int n = 0; n < number; n++) {
-                nodes[n] = Node.readNextLine(reader);
-                nodes[n] += ' ' + Node.readNextLine(reader);
-            }
-            System.out.println(Arrays.toString(nodes));
-        }
-        return nodes;
+    // Send START message
+    public static String sendStartRequest(BufferedReader in, BufferedWriter out, String nodeName) {
+        send(out, "START " + 1 + ' ' + nodeName);
+        return readNextLine(in);
     }
 
-    public static void end(BufferedWriter writer, String reason) {
-        send(writer, "END " + reason);
+    // Send END message
+    public static void sendEndRequest(BufferedWriter out, String reason) {
+        send(out, "END " + reason);
+    }
+
+    public static String sendEchoMessage(BufferedReader in, BufferedWriter out) {
+        send(out, "ECHO");
+        return readNextLine(in);
+    }
+
+    public static String sendPutRequest(BufferedReader in, BufferedWriter out, String key, String value) {
+        // Split the key and value into individual words
+        String[] keyParts = key.split("\n");
+        if (keyParts.length < 1) System.err.println("Blank key entered.");
+        String[] valueParts = value.split("\n");
+        if (valueParts.length < 1) System.err.println("Blank value entered.");
+
+        // Send PUT request
+        send(out, "PUT? " + keyParts.length + ' ' + valueParts.length + '\n' + key + value);
+        return readNextLine(in);
+    }
+
+    public static String sendGetRequest(BufferedReader in, BufferedWriter out, String key) {
+        // Split the key into individual words
+        String[] keyParts = key.split("\n");
+        if (keyParts.length < 1) System.err.println("Empty key entered.");
+
+        // Send GET request
+        send(out, "GET? " + keyParts.length + '\n' + key);
+        return readNextLine(in);
+    }
+
+    public static String sendNotifyRequest(BufferedReader in, BufferedWriter out, String nodeName, String nodeAddress) {
+        send(out, "NOTIFY?" + '\n' + nodeName + '\n' + nodeAddress);
+        return readNextLine(in);
+    }
+
+    public static List<FullNodeInfo> sendNearestRequest(BufferedReader in, BufferedWriter out, String hashID) {
+        List<FullNodeInfo> nodes = null;
+        Node.send(out, "NEAREST? " + hashID);
+        String response = Node.readNextLine(in);
+
+        // Check the response
+        if (response.startsWith("NODES")) {
+            int nodeCount = Integer.parseInt(response.split(" ")[1]);
+            nodes = new ArrayList<>(nodeCount);
+            for (int n = 0; n < nodeCount; n++) {
+                String nodeName = readNextLine(in);
+                String nodeAddress = readNextLine(in);
+                nodes.add(new FullNodeInfo(nodeName, nodeAddress));
+            }
+            System.out.println(nodes);
+        } else {
+            System.err.println("Unexpected response."); // TODO: END
+        }
+        return nodes;
     }
 }
